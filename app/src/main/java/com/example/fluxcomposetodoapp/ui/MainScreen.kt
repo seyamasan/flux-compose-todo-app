9a30d24325687fb9c1bd2f84ed22cdae1069d18b
seyamasan <kst_apr_24@icloud.com>
2025-04-28 00:13:49 +0900

    Created MainScreen.

---
+++ app/src/main/java/com/example/fluxcomposetodoapp/ui/MainScreen.kt
@@ -0,0 +1,117 @@
+package com.example.fluxcomposetodoapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fluxcomposetodoapp.ui.theme.FluxComposeTodoAppTheme
+
+@Composable
+fun MainScreen(
+    inputText: String,
+    onInputChange: (String) -> Unit,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onAddClick: () -> Unit,
+    onClearCompletedClick: () -> Unit,
+    itemList: List<String>
+) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Checkbox(
+                        checked = isChecked,
+                        onCheckedChange = onCheckedChange
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    TextField(
+                        value = inputText,
+                        onValueChange = onInputChange,
+                        modifier = Modifier
+                            .weight(1f),
+                        placeholder = { Text(text = "What needs to be done?") },
+                        maxLines = 1,
+                        singleLine = true
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Button(
+                        onClick = onAddClick
+                    ) {
+                        Text(text = "Add")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    items(itemList) { item ->
+                        Text(
+                            text = item,
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+            }
+
+            Button(
+                onClick = onClearCompletedClick,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Clear completed")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FluxComposeTodoAppTheme {
+        MainScreen(
+            inputText = "Test input",
+            onInputChange = {},
+            isChecked = false,
+            onCheckedChange = {},
+            onAddClick = {},
+            onClearCompletedClick = {},
+            itemList = listOf()
+        )
+    }
+}
